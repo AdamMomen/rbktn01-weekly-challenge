0cdc6508975486618649880da3bad23e8e327c41
AdamMomen <adammuman81@gmail.com>
2019-12-02 09:56:07 +0100

    Solution to Q6

--- assessment.js
+++ assessment.js
@@ -43,27 +43,27 @@ function daBears() {
 // Which function(s) access the "chair" variable and get "Too Big!"
 // (Delete wrong answers, leave correct ones)
 
-var fairyTale1 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
+var fairyTale1 = ["papaBear"];
 
 // Which function(s) access the "feeling" variable and get "Hungry"
 // (Delete wrong answers, leave correct ones)
 
-var fairyTale2 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
+var fairyTale2 = ["goldilocks"];
 
 // Which function(s) access the "porridge" variable and get "Too Cold!"
 // (Delete wrong answers, leave correct ones)
 
-var fairyTale3 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
+var fairyTale3 = ["mamaBear"];
 
 // Which function(s) access the "sleepy" variable and get undefined
 // (Delete wrong answers, leave correct ones)
 
-var fairyTale4 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
+var fairyTale4 = ["daBears", "papaBear", "mamaBear", "goldilocks"];
 
 // Which function(s) access the isFurry variable and get true
 // (Delete wrong answers, leave correct ones)
 
-var fairyTale5 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
+var fairyTale5 = ["daBears"];
 
 
 // *************
@@ -82,10 +82,20 @@ var fairyTale5 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
 // "charger" once, and invoke it twice on "mustang".
 
 // CODE HERE...
+//psuedoclassical constructor
+var Vehicle = function () {
+    this.gasRemaining = 100;
+}
+Vehicle.prototype.drive = function function_name(argument) {
+    this.gasRemaining -= 25;
+}
 
+var mustang  = new Vehicle();
+var charger  = new Vehicle();
 
-
-
+mustang.drive();
+mustang.drive();
+charger.drive();
 
 // -----------------------------------------------------------------------------
 
@@ -105,7 +115,18 @@ var fairyTale5 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
 // and should neither modify them nor break when encountering them.
 
 
+string.prototype.grammarPolice = function (string) {
+ 
+var copyOfStr = string.slice().toLowerCase().split();
 
+function capFirstLetter(word) {
+      word[0].toUpperCase();
+}
+    for (var i = 0; i < copyOfStr.length; i++) {
+        capFirstLetter(copyOfStr[i]);
+    }
+    return copyOfStr.join(' ');
+};
 
 // CODE HERE...
 
@@ -126,6 +147,18 @@ var fairyTale5 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
 // In all other cases, return "Different values"
 
 // CODE HERE...
+function valueType(arg1, arg2) {
+
+   if (arg1 === arg2) {
+        return "Exactly the same";
+    }
+   if (arg1 ==  arg2  && typeof()typeof()) {
+            return "Same value, different types";
+    } else {
+        return "Different values";
+    }
+}
+
 
 // *************
 // * PROBLEM 5 *
@@ -149,7 +182,10 @@ var elephant = {
 }
 function large() {
 
-    return 'My name is ' + this.name + ' and I am very heavy!'
+    return 'My name is ' + this.name.bind(elephant) + ' and I am very heavy!'
+}
+var boundToElephant =  function () {
+  return large.call(elephant);
 }
   // CODE HERE...
 
